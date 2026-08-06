4ad53af24d17dcf3d3c7db5be78161f39b3ac22e
kevfornal <kevfornal@gmail.com>
2026-08-05 20:15:55 -0400

    Update smb_finder.py

--- smb_finder.py
+++ smb_finder.py
@@ -11,10 +11,10 @@ from bs4 import BeautifulSoup
 # ==============================================================================
 MIN_CASH_FLOW = 100_000         # Min Annual Net Profit / Cash Flow ($100k)
 MIN_PRICE = 500_000             # Min Asking Price ($500k)
-MAX_PRICE = 2_000_000           # Max Asking Price ($2,000,000)
+MAX_PRICE = 8_000_000           # Max Asking Price ($2,000,000)
 MIN_YEARS_ESTABLISHED = 5       # Min Established Age (5+ Years)
 MIN_CF_MULTIPLE = 2.0           # Min Cash Flow Multiple (2.0x)
-MAX_CF_MULTIPLE = 4.0           # Max Cash Flow Multiple (4.0x)
+MAX_CF_MULTIPLE = 10.0           # Max Cash Flow Multiple (4.0x)
 
 # CONFIGURATION FROM SECRETS
 SMTP_SERVER = os.environ.get("SMTP_SERVER", "smtp.gmail.com")
